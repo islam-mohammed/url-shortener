6ebac7b78be2f49f013df09d2b139fc09bbc7346
Islam Mohammed <islammohammed1978@gmail.com>
2023-01-23 00:28:39 +0200

    Create the view function to increase the number of views by 1 and redirect the user to the original URL

--- app/Http/Controllers/ShortLinkController.php
+++ app/Http/Controllers/ShortLinkController.php
@@ -9,6 +9,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
 class ShortLinkController extends Controller
 
 {
@@ -22,9 +24,31 @@ class ShortLinkController extends Controller
         return Inertia::render('ShortLinks', [
             'shortLinks' => new ShortLinksCollection(
                 Auth::user()->shortLinks()
-                            ->orderBy('destination')
+                            ->latest()
                             ->paginate()
                             ->appends(request()->all()))
         ]);
+    }
+
+    /**
+     * Redirect the user to the original link.
+     *
+     * @param  App\Models\ShortLink;  $request
+     * @return \Illuminate\Routing\Redirector|\Illuminate\Http\RedirectResponse
+     */
+    public function view(ShortLink $shortlink)
+    {
+        if($shortlink != null) {
+            // increment the views by 1
+            $shortlink->views += 1;
+            // save the short link
+            $shortlink->save();
+            // redirect the user to the original link
+            return Redirect($shortlink->destination);
+        }
+
+        report(new NotFoundHttpException('This link in not valid'));
+        return false;
+
     }
 }
