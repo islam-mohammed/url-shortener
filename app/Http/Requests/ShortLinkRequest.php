15ea481e8977c7544a308bf907f574754be3aa69
Islam Mohammed <islammohammed1978@gmail.com>
2023-01-22 19:56:58 +0200

    Create the short link request

---
+++ app/Http/Requests/ShortLinkRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\ShortLink;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class ShortLinkRequest extends FormRequest
+{
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+         return [
+            'destination' => ['required', 'url', 'max:255', Rule::unique(ShortLink::class)->ignore($this->user()->id)],
+            'slug' => ['required', 'string', 'min:5', 'max:5'],
+        ];
+    }
+}
