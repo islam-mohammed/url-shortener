53608b138e6a33dd9ee96077db2fd58451452577
Islam Mohammed <islammohammed1978@gmail.com>
2023-01-22 22:34:23 +0200

    Testing | Add a test case to test if the  short links page is displayed

--- tests/Feature/ShortLinkTest.php
+++ tests/Feature/ShortLinkTest.php
@@ -12,15 +12,25 @@ use Tests\TestCase;
 class ShortLinkTest extends TestCase
 {
      public function test_short_link_accessor_is_valid() {
-        $slug = Str::random(5);
+
         $user = User::factory()->create();
         $url= ShortLink::factory()->state([
-            'slug' => $slug,
             'user_id' => $user->getKey(),
         ])->create();
+
+        $slug = $url->slug;
+        $this->assertNotNull($slug);
 
         $expectedUrl = url('') . '/' . $slug;
 
         $this->assertSame($expectedUrl, $url->shortLink);
+    }
+
+    public function test_short_links_page_is_displayed() {
+        $user = User::factory()->create();
+        $response = $this
+            ->actingAs($user);
+        $response = $this->get('/short');
+        $response->assertStatus(200);
     }
 }
