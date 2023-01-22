96519b3da14a3be0b59d7dce93393f6bf69c917d
Islam Mohammed <islammohammed1978@gmail.com>
2023-01-22 18:18:46 +0200

    Create the welcome page and dashboard routes

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,17 @@ use Inertia\Inertia;
 |
 */
 
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+    ]);
+});
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
