0b59df9ce978c19385e85e0a53d7ab9748722218
Islam Mohammed <islammohammed1978@gmail.com>
2023-01-22 16:16:30 +0200

    Add the profile routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,5 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 require __DIR__.'/auth.php';
