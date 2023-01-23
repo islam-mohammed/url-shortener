2c351a025378133771415a82721dbe3b9a3fd51e
Islam Mohammed <islammohammed1978@gmail.com>
2023-01-23 04:56:53 +0200

    Update the short link request validation rules to validate unique links per user

--- app/Http/Requests/ShortLinkRequest.php
+++ app/Http/Requests/ShortLinkRequest.php
@@ -5,6 +5,7 @@ namespace App\Http\Requests;
 use App\Models\ShortLink;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
+use Whoops\Run;
 
 class ShortLinkRequest extends FormRequest
 {
@@ -17,9 +18,26 @@ class ShortLinkRequest extends FormRequest
     public function rules()
     {
          return [
-            'destination' => ['required', 'url', 'max:255', Rule::unique(ShortLink::class)->ignore($this->user()->id)],
-            'slug' => ['required', 'string', 'min:5', 'max:5'],
-            'user_id' => ['required']
+            'destination' => ['required', 'url', 'max:255', Rule::unique('short_links')->where(fn($query) => $query->where('user_id', $this->user_id)
+                ->where('destination', $this->destination)
+            )],
+            'user_id' => ['required', 'exists:App\Models\User,id']
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'destination.url' => 'Please enter a valid URL',
+            'destination.required' => 'Please enter a valid URL',
+            'destination.unique' => 'Please enter a unique URL',
+            'user_id.required' => 'Please enter a user id',
+            'user_id.exists' => 'The user id does not exist'
         ];
     }
 }
